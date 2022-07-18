6c2e138890f00e7393df92f9b16b2a7942aba6a9
Umrikhin <bono39@yandex.ru>
2022-07-18 21:38:57 +0300

    update projects FeedBack and FeedBackCaptcha

--- chapter 9/FeedBackCaptcha/FeedBack/Models/SendFormFeedback.cs
+++ chapter 9/FeedBackCaptcha/FeedBack/Models/SendFormFeedback.cs
@@ -6,18 +6,18 @@ namespace FeedBack.Models
     {
         [Display(Name = "Ваше имя")]
         [Required(ErrorMessage = "Сообщите Ваше имя.")]
-        [StringLength(50, ErrorMessage = "Пожайлуста, внесите не более 50 символов.")]
+        [StringLength(50, ErrorMessage = "Пожалуйста, внесите не более 50 символов.")]
         public string IM { get; set; } = string.Empty;
 
         [Display(Name = "Ваш Email")]
-        [Required(ErrorMessage = "Пожайлуста, внесите действующий E-mail.")]
+        [Required(ErrorMessage = "Пожалуйста, внесите действующий E-mail.")]
         [EmailAddress(ErrorMessage = "Внесите правильный E-mail")]
-        [StringLength(50, ErrorMessage = "Пожайлуста, внесите не более 50 символов.")]
+        [StringLength(50, ErrorMessage = "Пожалуйста, внесите не более 50 символов.")]
         public string Email { get; set; } = string.Empty;
 
         [Display(Name = "Тема")]
-        [Required(ErrorMessage = "Внесите тему сообщения. Пожайлуста, внесите не менее 8 символов.")]
-        [StringLength(50, ErrorMessage = "Пожайлуста, внесите не более 50 символов.")]
+        [Required(ErrorMessage = "Внесите тему сообщения. Пожалуйста, внесите не менее 8 символов.")]
+        [StringLength(50, ErrorMessage = "Пожалуйста, внесите не более 50 символов.")]
         public string Sbj { get; set; } = string.Empty;
 
         [Display(Name = "Ваше сообщение")]
